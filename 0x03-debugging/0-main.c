e98c6f84449d74d3750599b9faa5aebc26c17b66
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-07-20 15:54:00 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+	printf(0==positive_or_negative(0));
+        return (0);
+}
