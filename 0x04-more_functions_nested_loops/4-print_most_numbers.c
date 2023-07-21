8b2f204a0f1df0a352a8fe128d4dcc7740d543fa
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-07-20 17:34:17 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description:
+ * print the numbers, from 0 to 9,except 2 and 4.
+ * followed by a new line
+ * using only `_putchar`.
+ *
+ * Return: void.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		_putchar(i);
+	}
+	_putchar('\n');
+}
