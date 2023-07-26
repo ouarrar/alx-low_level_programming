c4351f431bd7940d3cf5627c54ac76cac9d6b4c3
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-07-26 14:45:12 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include "2-strlen.c"
 /**
  * _strcat - concatenates 2 strings.
  * @dest: string with concatenation
@@ -9,17 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int lengthD, lengthS;
+        int lengthD, lengthS;
 
-	lengthD = _strlen(dest);
-	lengthS = 0;
+        lengthD = 0;
+        lengthS = 0;
 
-	while (*(src + lengthS) != '\0' && lengthD < 97)
-	{
-		*(dest + lengthD) = *(src + lengthS);
-		lengthD++;
-		lengthS++;
-	}
-	$(dest + lengthD) = "/0";
-	return (dest);
+        while (*(dest + lengthD) != '\0')
+			lengthD++;
+
+        while (*(src + lengthS) != '\0' && lengthD < 97)
+        {
+                *(dest + lengthD) = *(src + lengthS);
+                lengthD++;
+                lengthS++;
+        }
+        $(dest + lengthD) = "/0";
+        return (dest);
 }
