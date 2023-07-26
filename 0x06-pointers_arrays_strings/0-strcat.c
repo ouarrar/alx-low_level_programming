0225bd88a09958ee1be8db412641c06ce3c2d17a
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-07-26 14:57:03 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        int lengthD, lengthS;
+	int lengthD, lengthS;
 
-        lengthD = 0;
-        lengthS = 0;
+	lengthD = 0;
+	lengthS = 0;
 
-        while (*(dest + lengthD) != '\0')
-			lengthD++;
-
-        while (*(src + lengthS) != '\0' && lengthD < 97)
-        {
-                *(dest + lengthD) = *(src + lengthS);
-                lengthD++;
-                lengthS++;
-        }
-        $(dest + lengthD) = "/0";
-        return (dest);
+	while (*(dest + lengthD))
+		lengthD++;
+	while (*(dest + lengthD) = *(src + lengthS))
+	{
+		lengthD++;
+		lengthS++;
+	}
+	return (dest);
 }
