978d21eb447a9283b8362c04090e669c35d3634f
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-07-31 15:39:40 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: string
+ * @c: character
+ * Return: pointer to the first occurrence of @c in @s,
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
