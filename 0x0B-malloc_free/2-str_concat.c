3c30682c4d4befc695a2d56a520a72bea45fdc8d
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-08-08 14:45:31 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,23 +11,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concatenated;
+	unsigned int len1;
+	unsigned int len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	unsigned int len1;
-
 	len1 = 0;
-
 	while (s1[len1] != '\0')
 		len1++;
 
-	unsigned int len2;
-
 	len2 = 0;
-
 	while (s2[len2] != '\0')
 		len2++;
 
