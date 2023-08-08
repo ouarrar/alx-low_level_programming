fc8cf9466c66d99e221efbee2ff5804e1ded9fab
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-08-08 13:49:56 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	char *array = (char *)malloc(size * sizeof(char));
+	char *array;
 	unsigned int i;
 
+	array = (char *)malloc(size * sizeof(char));
 	if (array != NULL)
 	{
 		for (i = 0; i < size; i++)
