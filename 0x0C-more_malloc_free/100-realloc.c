6f22ca25668e3f5bf2e1ee44f23cfb26fa8a473d
ouarrar.abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-08-11 00:23:19 +0100

    3-array_range.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+#include "memcpy.c"
+
+/**
+ * _realloc - Reallocates a memory block.
+ * @ptr: A pointer to the memory previously allocated with malloc.
+ * @old_size: The size, in bytes, of the allocated space for ptr.
+ * @new_size: The new size, in bytes, of the new memory block.
+ *
+ * Return: A pointer to the newly allocated memory block, or NULL if new_size is 0 and ptr is not NULL.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *new_ptr;
+    unsigned int size;
+
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
+
+    if (ptr == NULL)
+    {
+        new_ptr = malloc(new_size);
+        return (new_ptr);
+    }
+
+    if (new_size == old_size)
+    {
+        return ptr;
+    }
+
+    new_ptr = malloc(new_size);
+
+    if (new_ptr == NULL)
+    {
+        return (NULL);
+    }
+
+    if(old_size < new_size)
+    {
+        size = old_size
+    }
+    else{
+        size = new_size
+    }
+
+    _memcpy(new_ptr, ptr,size);
+
+    free(ptr);
+
+    return (new_ptr);
+}
