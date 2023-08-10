fb9951f01c55d86bd3472b1b44ad3003f1828bc1
ouarrar.abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-08-11 00:35:45 +0100

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,39 +5,39 @@
 
 int is_positive_number(const char *str)
 {
-    while (*str)
-    {
-        if (!isdigit(*str))
-        {
-            return (0);
-        }
-        str++;
-    }
-    return (1);
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned long long num1, num2, result;
+	unsigned long long num1, num2, result;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = strtoull(argv[1], NULL, 10);
-    num2 = strtoull(argv[2], NULL, 10);
+	num1 = strtoull(argv[1], NULL, 10);
+	num2 = strtoull(argv[2], NULL, 10);
 
-    result = num1 * num2;
+	result = num1 * num2;
 
-    printf("%llu\n", result);
+	printf("%llu\n", result);
 
-    return (0);
+	return (0);
 }
