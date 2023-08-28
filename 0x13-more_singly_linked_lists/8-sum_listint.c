ec8ab9435b0a77fa33eef71b49903fcff2e2df46
ouarrar.abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-08-29 00:38:17 +0100

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Calculates the sum of all integer values in a listint_t list.
+ * @head: A pointer to the head of the list.
+ *
+ * Description: This function traverses the listint_t linked list and calculates
+ * the sum of all the integer values contained in the nodes.
+ *
+ * Return: The sum of all integer values in the list.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
