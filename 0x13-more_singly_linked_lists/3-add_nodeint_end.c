949adafe3a3491ceab58c286185bad119d0f9cde
ouarrar.abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-08-29 00:20:56 +0100

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,8 +5,8 @@
  * @head: A pointer to a pointer to the head of the list.
  * @n: The integer value to be stored in the new node.
  *
- * Description: This function creates a new node with the given integer value (@n)
- *              and adds it to the end of the linked list pointed to by @head.
+ * Description: This function creates a new node with the given
+ * integer value (@n)and adds it to the end of the linked list pointed to by @head.
  *
  * Return: the address of the new element, or NULL if it failed
  */
