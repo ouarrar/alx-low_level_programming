ca66c6c533160beedc1179015d88017e72ca07e5
ouarrar.abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-09-01 00:09:17 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,25 @@
  *
  * Return: The converted number, or 0 if there are invalid characters or if b is NULL.
  */
-unsigned int binary_to_uint(const char *b) {
-    if (b == NULL)
-        return (0);
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return (0);
 
-    unsigned int result = 0;
-    int power = 0;
+	unsigned int result = 0;
+	int power = 0;
 
-    while (*b != '\0') {
-        if (*b != '0' && *b != '1') {
-            return (0); // Invalid character found
-        }
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0); // Invalid character found
+		}
 
-        result = (result << 1) | (*b - '0'); // Shift left and add the binary digit
-        b++;
-        power++;
-    }
+		result = (result << 1) | (*b - '0'); // Shift left and add the binary digit
+		b++;
+		power++;
+	}
 
-    return (result);
+	return (result);
 }
