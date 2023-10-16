c27b87e52308cd2ec670982c69ddf146a059a238
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-10-16 15:13:52 -0700

    libdynamic.so

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,3 +26,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+
